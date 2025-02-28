78ee35feca5be18696bb7eb67a89d060cfdf8a96
rajaram <rajaram9587319178@gmail.com>
2025-02-28 18:29:41 +0530

    Create API.

--- blog/views.py
+++ blog/views.py
@@ -63,7 +63,7 @@ def post_list(request):
             raise Http404("Invalid date format. Please use YYYY-MM-DD.")
 
     # Pagination
-    paginator = Paginator(posts, 20)  # Show 10 posts per page
+    paginator = Paginator(posts, 10)  # Show 10 posts per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
